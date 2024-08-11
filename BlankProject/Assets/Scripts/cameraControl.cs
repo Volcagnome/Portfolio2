7afc264916e9c4e4528b7241d6846f18a88873aa
Phillip Prado <146027013+PhillipFS@users.noreply.github.com>
2024-08-11 16:15:14 -0600

    Player and Cam controls
    Added basic player and cam control scripts with adjustable variables to tune movement.

---
+++ BlankProject/Assets/Scripts/cameraControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    float rotX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // turns off cursor, since we're using a fps view
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input:
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+
+        if (invertY)
+            rotX += mouseY;
+        else
+            rotX -= mouseY;
+
+        // Clamp the rotX on the x-axis
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        // Rotate the cam on the x-axis
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        // Rotate the player on the y-axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ BlankProject/Assets/Scripts/playerControl.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] LayerMask ignoreMask;
+
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpMax;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDist;
+
+    Vector3 move;
+    Vector3 playerVel;
+
+    int jumpCount;
+
+    bool isShooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            // Jump count reset
+            jumpCount = 0;
+            // Fall velocity is 0, since player is grounded
+            playerVel.y = 0;
+        }
+
+        else
+        {
+            // If not grounded, fall velocity increases
+            playerVel.y -= gravity * Time.deltaTime;
+        }
+
+        move = Input.GetAxis("Vertical") * transform.forward +
+               Input.GetAxis("Horizontal") * transform.right;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+
+        controller.Move(playerVel * Time.deltaTime);
+
+        if (Input.GetButton("Shoot") && !isShooting)
+            StartCoroutine(shoot());
+    }
+
+    void sprint()
+    {
+        // Sprint modifiers modify speed:
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hit;
+        // Physics.Raycast (Origin, Direction, hit info, max distance)
+        if (Physics.Raycast(Camera.main.transform.position,
+            Camera.main.transform.forward, out hit, shootDist, ~ignoreMask))
+        {
+            Debug.Log(hit.collider.name);
+        }
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+}
