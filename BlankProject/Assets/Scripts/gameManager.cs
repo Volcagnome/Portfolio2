8df03b88973f6f2d5f29616cd3526d48b2f263cd
SCCaraballo <135036321+SCCaraballo@users.noreply.github.com>
2024-08-12 18:56:54 -0500

    buttonFunctions
    created buttonFunctions script

--- BlankProject/Assets/Scripts/gameManager.cs
+++ BlankProject/Assets/Scripts/gameManager.cs
@@ -1,61 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class gameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public static gameManager instance;
+    public static GameManager instance;
 
-    [SerializeField] GameObject menuActive;
-    [SerializeField] GameObject menuPause;
-    [SerializeField] GameObject menuWin;
-    [SerializeField] GameObject menuLose;
-
-    public bool isPaused;
-
-
+    public GameObject player;
+    public playerControl playerScript;
 
     // Start is called before the first frame update
     void Awake()
     {
-
-
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerControl>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Cancel"))
-        {
-            if (menuActive == null)
-            {
-                statePause();
-                menuActive = menuPause;
-                menuActive.SetActive(isPaused);
-            }
-            else if (menuActive == menuPause)
-            {
-                stateUnpaused();
-            }
-        }
+        
     }
 
-    public void statePause()
-    {
-        isPaused = !isPaused;
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.Confined;
-    }
 
-    public void stateUnpaused()
-    {
-        isPaused = !isPaused;
-        Time.timeScale = 1;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        menuActive.SetActive(isPaused);
-        menuActive = null;
-    }
 }
