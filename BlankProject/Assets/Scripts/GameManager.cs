a93495eb90b26afa19324d002229a3a861fb8e19
SCCaraballo <135036321+SCCaraballo@users.noreply.github.com>
2024-08-14 13:42:39 -0500

    GameManager

--- BlankProject/Assets/Scripts/GameManager.cs
+++ BlankProject/Assets/Scripts/GameManager.cs
@@ -6,11 +6,17 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+
+
     public GameObject player;
     public playerControl playerScript;
     public List<GameObject> levers_List;
     private int activeLevers;
     public bool youWin;
+
+    public bool isPaused;
 
     // Start is called before the first frame update
     void Awake()
@@ -23,7 +29,37 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpaused();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
     }
 
     public void AddLeverToList(GameObject lever)
