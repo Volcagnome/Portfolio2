218909d44cd9d9ff9c9611474a38fc9a81bcff51
AdamsChristopher04 <151537568+AdamsChristopher04@users.noreply.github.com>
2024-08-23 14:19:29 -0400

    Security Camera
    Attach a button or a lever to turn it on and off and break the object with a single shot. The cone is a default cone from Blender, and naturally imported directly from there.

---
+++ BlankProject/Assets/Scripts/objectFunctions/securityCamera.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static enemyAI;
+
+public class securityCamera : MonoBehaviour, IDamage, IToggle
+{
+    [SerializeField] GameObject cameraHead;
+    [SerializeField] GameObject controller;
+    [SerializeField] GameObject searchPoint;
+    [SerializeField] float alarmRadius;
+    [SerializeField] Camera referenceCamera;
+
+    bool isActive;
+
+    //Fields lifted directly from Lynsey's enemyAI script
+    [SerializeField] float FOV;
+    [SerializeField] LayerMask targetMask;
+    [SerializeField] LayerMask enemyLayer;
+    public bool playerInView;
+
+
+    void Start()
+    {
+        if (controller != null) isActive = controller.GetComponent<ISendState>().getState();
+    }
+
+    
+    void Update()
+    {
+        if (cameraHead != null)
+        {
+            if (controller != null && controller.GetComponent<ISendState>().getState() != isActive) toggle(controller.GetComponent<ISendState>().getState());
+
+            if (playerInView) AlertEnemies();
+
+            CheckAngle();
+        }
+        else if (playerInView) playerInView = false;
+    }
+    public void toggle(bool state)
+    {
+        isActive = state;
+    }
+
+    public void takeDamage(int dmg)
+    {
+        if (cameraHead != null) Destroy(cameraHead);
+        playerInView = false;
+    }
+    public void criticalHit(int dmg)
+    {
+        takeDamage(dmg);
+    }
+
+    //Copied directly from another chunk of Lynsey's code becuase I'm too lazy to be original here
+    public void AlertEnemies()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(gameObject.transform.position, alarmRadius, enemyLayer);
+
+        if (enemiesInRange.Length > 0)
+        {
+            foreach (Collider foe in enemiesInRange)
+            {
+                foe.gameObject.GetComponent<enemyAI>().AlertEnemy();
+            }
+        }
+    }
+    //Taken from lecture code
+    void CheckAngle()
+    {
+        Vector3 playerDirection = GameManager.instance.player.transform.position - searchPoint.transform.position;
+        float angleToPlayer = Vector3.Angle(playerDirection, searchPoint.transform.forward);
+
+        
+
+        RaycastHit hit;
+        if (Physics.Raycast(searchPoint.transform.position, playerDirection, out hit, alarmRadius, ~enemyLayer))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= FOV)
+            {
+                playerInView = true;
+            }
+            else playerInView = false;
+        }
+        else playerInView = false;
+    }
+}
