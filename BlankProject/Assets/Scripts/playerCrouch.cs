d54f7f805ab2a79f4a9e9958bdf1fa300f26fa97
Phillip Prado <146027013+PhillipFS@users.noreply.github.com>
2024-08-25 11:59:02 -0600

    Crouch and Prone
    Player can now crouch and stand up again by pressing the left ctrl button. Holding it for 1.5 seconds causes the player to go prone. UI was added to indicate the player's crouch status. Whenever a player crouches or enters prone, the player's 'hurtbox' parented object is moved down locally. Player receives damage from enemies shooting this collider, instead of the entire mesh.
    
    playerControl.cs was separated into 3 scripts: playerMovement (contains movement and sprint code), playerCrouch (contains hitbox collider and crouch/prone code) and playerDamage. (contains HP and shooting related code)

---
+++ BlankProject/Assets/Scripts/playerCrouch.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCrouch : MonoBehaviour
+{
+    [SerializeField] float standingHeight;
+    [SerializeField] float crouchHeight;
+    [SerializeField] float proneHeight;
+
+    [SerializeField] float proneTime;
+
+    float heightOG;
+    bool isCrouched;
+    float holdCrouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crouch();
+    }
+
+    void crouch()
+    {
+        if (Input.GetButtonDown("Crouch") && isCrouched == false)
+        {
+            // On button press, lowers player height to crouch height.
+            transform.localPosition = new Vector3 (0, crouchHeight, 0);
+           
+            // Turn on indicator. (crouch true, prone false)
+            GameManager.instance.crouchWindow.SetActive(true);
+            GameManager.instance.proneWindow.SetActive(false);
+
+            isCrouched = true;
+            
+        }
+
+        else if (Input.GetButtonDown("Crouch") && isCrouched == true)
+        {
+            // On key up, returns to regular height.
+            transform.localPosition = new Vector3(0, standingHeight, 0);
+
+            // Turn off indicator. (both false)
+            GameManager.instance.crouchWindow.SetActive(false);
+            GameManager.instance.proneWindow.SetActive(false);
+
+            isCrouched = false;
+        }
+
+        // *** PRONE *** //
+        if (Input.GetButton("Crouch"))
+        {
+            // Count towards prone time requirement.
+            holdCrouch += Time.deltaTime;
+            // If met, enter prone mode:
+            if (holdCrouch >= proneTime)
+            {
+                // Transform:
+                transform.localPosition = new Vector3(0, proneHeight, 0);
+
+                holdCrouch = 0;
+
+                // Turn on indicator. (prone true, crouch false)
+                GameManager.instance.proneWindow.SetActive(true);
+                GameManager.instance.crouchWindow.SetActive(false);
+
+                isCrouched = true;
+            }
+
+            if (Input.GetButtonUp("Crouch"))
+            {
+                // If prone time is not enough and player lets go of key, reset count timer.
+                holdCrouch = 0;
+            } 
+        }
+    }
+
+}
+
+    ////////////////////////
+    // *** CROUCHING *** //
+    ///////////////////////
+    /*
+    if (Input.GetButtonDown("Crouch") && isCrouched == false)
+    {
+        // On button press, lowers player height to crouch height.
+        playerPosition.transform.Translate(0, crouchHeight, 0);
+
+        isCrouched = true;
+    }
+
+    else if (Input.GetButtonDown("Crouch") && isCrouched == true)
+    {
+        // On key up, returns to regular height.
+        playerPosition.transform.Translate(0, heightOG, 0);
+        isCrouched = false;
+    }
+
+    // PRONE (Commented out temporarily)
+    if (Input.GetButton("Crouch"))
+    {
+        holdCrouch += Time.deltaTime;
+        if (holdCrouch >= proneTime)
+        {
+            controller.height = proneHeight;
+            holdCrouch = 0;
+            isCrouched = true;
+        }
+    }*/
+
