0199d7a9ee6f1da9e5ee37f4f463e5ab902dcf49
MathiasVer2055 <mcmorris1@student.fullsail.edu>
2024-08-28 21:55:38 -0700

    void

--- BlankProject/Assets/Scripts/enviromentHazard.cs
+++ BlankProject/Assets/Scripts/enviromentHazard.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class enviromentHazard : MonoBehaviour
 {
-    [SerializeField] enum damageType { stationary, trap }
+    [SerializeField] enum damageType { stationary, trapSet3, trapSet2 }
     [SerializeField] damageType type;
     [SerializeField] Rigidbody rb;
-    [SerializeField] GameObject itemOnState;
-    [SerializeField] GameObject itemOffState;
+    [SerializeField] GameObject itemOnOneState;
+    [SerializeField] GameObject itemOnTwoState;
+    [SerializeField] GameObject itemOnThreeState;
+    [SerializeField] GameObject itemOffOneState;
+    [SerializeField] GameObject itemOffTwoState;
+    [SerializeField] GameObject itemOffThreeState;
 
     [SerializeField] int damageAmount;
 
@@ -27,10 +31,23 @@ public class enviromentHazard : MonoBehaviour
             dmg.takeDamage(damageAmount);
 
         }
-        else if (dmg != null && other.gameObject.tag != "Enemy" && type == damageType.trap)
+        //Activate all 3 spikes simutaneously(to be used with set of three spike trap)
+        else if (dmg != null && other.gameObject.tag != "Enemy" && type == damageType.trapSet3)
         {
-            itemOnState.SetActive(true);
-            itemOffState.SetActive(false);
+            itemOnOneState.SetActive(true);
+            itemOnTwoState.SetActive(true);
+            itemOnThreeState.SetActive(true);
+            itemOffOneState.SetActive(false);
+            itemOffTwoState.SetActive(false);
+            itemOffThreeState.SetActive(false);
+            dmg.takeDamage(damageAmount);
+        }
+        //Activate spikes indepndently(to be used with set of two, add script to spikes -not trap empty object parent and
+        //set other states to an empty game object)
+        else if (dmg != null && other.gameObject.tag != "Enemy" && type == damageType.trapSet2)
+        {
+            itemOnOneState.SetActive(true);
+            itemOffOneState.SetActive(false);
             dmg.takeDamage(damageAmount);
         }
 
