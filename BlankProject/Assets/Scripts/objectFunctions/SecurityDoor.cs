3399ed0b032f2321dbffaa119a78fa21d85b79be
MathiasVer2055 <mcmorris1@student.fullsail.edu>
2024-09-22 21:28:03 -0700

    Revert "UI Variant"
    This reverts commit e44f589be777f28124f678e0d6d186a9339c06f3.

--- BlankProject/Assets/Scripts/objectFunctions/SecurityDoor.cs
+++ BlankProject/Assets/Scripts/objectFunctions/SecurityDoor.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 
 public class SecurityDoor : toggleReciever
 {
