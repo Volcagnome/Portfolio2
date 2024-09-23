56cb1944e55d6293a48d3e506ffe6c1b77399a63
Phillip Prado <146027013+PhillipFS@users.noreply.github.com>
2024-09-23 14:49:23 -0600

    Camera control bug fix
    Fixed a bug where the camera suddenly moves downward or upward fully.

--- BlankProject/Assets/Scripts/cameraControl.cs
+++ BlankProject/Assets/Scripts/cameraControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] int sens;
-    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] float lockVertMin, lockVertMax;
     [SerializeField] bool invertY;
 
     float rotX;
@@ -22,19 +22,22 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         // Get input:
-        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
-        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens;
+        float mouseX = Input.GetAxis("Mouse X") * sens;
+        //float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        //float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
 
         if (invertY)
             rotX += mouseY;
         else
             rotX -= mouseY;
 
-        // Clamp the rotX on the x-axis
+        // Clamp the rotX on the x-axis:
         rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
 
-        // Rotate the cam on the x-axis
-        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+        // Rotate the cam on the x-axis:
+        transform.localEulerAngles = Vector3.right * rotX;
+        //transform.localRotation = Quaternion.Euler(rotX, 0, 0);
 
         // Rotate the player on the y-axis
         transform.parent.Rotate(Vector3.up * mouseX);
